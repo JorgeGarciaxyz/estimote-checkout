46bc0080c4b95d129f48cd8b30dd5255f626b718
Jorge Garcia <hegg777@gmail.com>
2016-10-11 20:53:19 -0500

    Create edit action and view, missing form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit]
+
   def index
     @users = User.all.order(:last_name)
   end
+
+  def edit
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
 end
