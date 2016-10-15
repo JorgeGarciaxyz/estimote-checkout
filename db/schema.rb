e126eb497cf670dfd5f6862405f5e9ff1bd4ecc5
Jorge Garcia <hegg777@gmail.com>
2016-10-15 14:47:55 -0500

    Change user_id integer from lessons table, using references instead

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005011043) do
+ActiveRecord::Schema.define(version: 20161015193228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lessons", force: :cascade do |t|
+    t.string   "name"
+    t.string   "hour"
+    t.string   "classroom"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_lessons_on_user_id", using: :btree
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "full_name"
@@ -42,4 +52,5 @@ ActiveRecord::Schema.define(version: 20161005011043) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "lessons", "users"
 end
