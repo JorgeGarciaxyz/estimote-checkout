28d55bb568403573b4b780cf8a323fb62ccf8a43
Rolando Islas <dani.islas96@gmail.com>
2016-10-15 19:34:37 -0500

    Make post action for assistances

--- app/controllers/assistances_controller.rb
+++ app/controllers/assistances_controller.rb
@@ -1,2 +1,29 @@
-class AssistancesController < ApplicationController
+class AssistancesController < ApplicationApiController
+
+  include ValidateUserInLesson
+
+  before_action :set_enrolled_student, only: [:create]
+
+  def create
+    if uuid_is_valid(create_params[:uuid])
+      student_is_in_class = is_student_in_lesson(@enrolled_student, create_params[:lesson_id])
+      if student_is_in_class
+        Assistance.create(is_present: create_params[:is_present],
+                          enrolled_student_id: @enrolled_student.id)
+      end
+    end
+  end
+
+  private
+
+  def set_enrolled_student
+    if uuid_is_valid(create_params[:uuid])
+      student = Student.find_by(uuid: create_params[:uuid])
+      @enrolled_student = EnrolledStudent.find_by(student_id: student.id)
+    end
+  end
+
+  def create_params
+    params.require(:assistance).permit(:is_present, :uuid, :lesson_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users, except: [:show]
   resources :students
   resources :lessons
+  resources :assistances, only: [:create]
 
   get '/students_menu' => "statics#students_menu"
   get '/lessons_menu' => "statics#lessons_menu"
