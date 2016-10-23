2c856dcd8114958a623a34ee5cd63d65657b5356
Jorge Garcia <hegg777@gmail.com>
2016-10-23 17:21:30 -0500

    Refactor assistance create and clean up json format on absence

--- app/controllers/assistances_controller.rb
+++ app/controllers/assistances_controller.rb
@@ -9,15 +9,14 @@ class AssistancesController < ApplicationApiController
       student_is_in_class = is_student_in_lesson(@enrolled_student, create_params[:lesson_id])
       if student_is_in_class
         if !(Assistance.where("created_at >= ?", Time.zone.today.beginning_of_day).where(enrolled_student_id: @enrolled_student.id).any?)
-          Assistance.create(is_present: create_params[:is_present],
-                            enrolled_student_id: @enrolled_student.id)
+          Assistance.create(is_present: true, enrolled_student_id: @enrolled_student.id)
         end
       end
     end
   end
 
   def set_false_assistances
-    lesson = Lesson.find(params[:id])
+    lesson = Lesson.find(create_params[:lesson_id])
     lesson.enrolled_students.each do |enrolled_student|
       if !(Assistance.where("created_at >= ?", Time.zone.today.beginning_of_day).where(enrolled_student_id: enrolled_student.id).any?)
         Assistance.create(is_present: false, enrolled_student_id: enrolled_student.id)
@@ -40,6 +39,6 @@ class AssistancesController < ApplicationApiController
   end
 
   def create_params
-    params.require(:assistance).permit(:is_present, :uuid, :lesson_id)
+    params.require(:assistance).permit(:uuid, :lesson_id)
   end
 end
